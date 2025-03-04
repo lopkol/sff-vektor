a38d2ad59857712d55d52d7b7d69e559539738e4
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-03-04 22:36:33 +0100

    revert: remove files added by error

--- lib/config/database.ts
+++
@@ -1,8 +0,0 @@
-import { createPool } from "slonik";
-import { createPgDriverFactory } from "@slonik/pg-driver";
-
-export async function createDbPool() {
-  return await createPool(Deno.env.get("DATABASE_URL")!, {
-    driverFactory: createPgDriverFactory(),
-  });
-}
