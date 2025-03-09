a86ba94a236a5d6ff5f8be927be52b8312ea6b0e
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-03-09 16:31:08 +0100

    chore: update deno.json of all projects + fix tests

--- lib/helpers/crypto_test.ts
+++ lib/helpers/crypto_test.ts
@@ -1,8 +1,17 @@
 import { assertEquals } from "@std/assert";
-import { hashEmail, encrypt, decrypt } from "./crypto.ts";
+import { hashEmail, encrypt, decrypt } from "@/helpers/crypto.ts";
+import { beforeAll } from "@std/testing/bdd";
+
+beforeAll(() => {
+  Deno.env.set("DATA_ENCRYPTION_KEY", "super-amazing-dev-secret");
+  Deno.env.set(
+    "EMAIL_SALT",
+    "kbGBDGpjjCsgCoCMDzs8yrZRdkykT4NivZMqla5Wa7htHJFHkohtU7OhZhinwKesb8HcAAskEfSNg4zg1iL3iUmJgkjaJ95jjACNwdR1ipaL39rk8pUpQS6M"
+  );
+});
 
 Deno.test(async function hashingIsDeterministic() {
-  const email = 'unicorn@magic.com';
+  const email = "unicorn@magic.com";
   const encryptedData = await hashEmail(email);
   const encryptedData2 = await hashEmail(email);
 
@@ -10,7 +19,7 @@ Deno.test(async function hashingIsDeterministic() {
 });
 
 Deno.test(async function encryptionIsReversible() {
-  const data = 'I like rainbows';
+  const data = "I like rainbows";
   const encryptedData = await encrypt(data);
   const decryptedData = await decrypt(encryptedData);
 
