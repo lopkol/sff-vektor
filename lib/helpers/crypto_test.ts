158cba1a8f3d5a510291ff3f820783ce0bee4427
lopkol <nora.szoke26@gmail.com>
2025-03-09 20:53:24 +0100

    test: write test descriptions for existing tests

--- lib/helpers/crypto_test.ts
+++ lib/helpers/crypto_test.ts
@@ -1,16 +1,16 @@
 import { assertEquals } from "@std/assert";
-import { hashEmail, encrypt, decrypt } from "@/helpers/crypto.ts";
+import { decrypt, encrypt, hashEmail } from "@/helpers/crypto.ts";
 import { beforeAll } from "@std/testing/bdd";
 
 beforeAll(() => {
   Deno.env.set("DATA_ENCRYPTION_KEY", "super-amazing-dev-secret");
   Deno.env.set(
     "EMAIL_SALT",
-    "kbGBDGpjjCsgCoCMDzs8yrZRdkykT4NivZMqla5Wa7htHJFHkohtU7OhZhinwKesb8HcAAskEfSNg4zg1iL3iUmJgkjaJ95jjACNwdR1ipaL39rk8pUpQS6M"
+    "kbGBDGpjjCsgCoCMDzs8yrZRdkykT4NivZMqla5Wa7htHJFHkohtU7OhZhinwKesb8HcAAskEfSNg4zg1iL3iUmJgkjaJ95jjACNwdR1ipaL39rk8pUpQS6M",
   );
 });
 
-Deno.test(async function hashingIsDeterministic() {
+Deno.test("hashEmail is deterministic", async () => {
   const email = "unicorn@magic.com";
   const encryptedData = await hashEmail(email);
   const encryptedData2 = await hashEmail(email);
@@ -18,7 +18,7 @@ Deno.test(async function hashingIsDeterministic() {
   assertEquals(encryptedData, encryptedData2);
 });
 
-Deno.test(async function encryptionIsReversible() {
+Deno.test("decrypt reverses encrypt", async () => {
   const data = "I like rainbows";
   const encryptedData = await encrypt(data);
   const decryptedData = await decrypt(encryptedData);
