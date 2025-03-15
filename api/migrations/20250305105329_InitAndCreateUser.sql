b871268ece2534dfbba9bac15c7c5c7b0f125fd2
lopkol <nora.szoke26@gmail.com>
2025-03-15 15:17:55 +0100

    feat: migration for book tables + improve user migration

--- api/migrations/20250305105329_InitAndCreateUser.sql
+++ api/migrations/20250305105329_InitAndCreateUser.sql
@@ -24,31 +24,28 @@ begin
 end
 $$ language plpgsql volatile;
 
--- reader
-create table public."reader" (
-    id            uuid not null default uuid_generate_v7() primary key,
-    moly_username character varying default null,
-    moly_url      character varying default null,
-    created_at    timestamp with time zone not null default now(),
-    updated_at    timestamp with time zone not null default now()
+create table if not exists "reader" (
+    id             uuid                     not null default uuid_generate_v7() primary key,
+    moly_username  varchar(1024)            default null,
+    moly_url       varchar(2048)            default null,
+    created_at     timestamp with time zone not null default now(),
+    updated_at     timestamp with time zone not null default now()
 );
 
--- user
-create table public."user" (
-    id              uuid not null default uuid_generate_v7() primary key,
-    email_hash      character varying not null,
-    email_encrypted character varying not null,
-    name            character varying default null,
-    role            character varying(255) not null default 'user',
-    is_active       boolean not null default true,
+create table if not exists "user" (
+    id              uuid                     not null default uuid_generate_v7() primary key,
+    email_hash      varchar(1024)            not null,
+    email_encrypted varchar(1024)            not null,
+    name            varchar(1024)            default null,
+    role            varchar(256)             not null default 'user',
+    is_active       boolean                  not null default true,
     created_at      timestamp with time zone not null default now(),
     updated_at      timestamp with time zone not null default now(),
-    reader_id       uuid default null,
-    constraint unique_user_email unique (email_hash),
-    constraint fk_user_reader_id foreign key (reader_id) references public."reader" (id)
+    reader_id       uuid                     default null references "reader" (id),
+    constraint unique_user_email unique (email_hash)
 );
 
 
 -- migrate:down
-drop table public."user";
-drop table public."reader";
+drop table "user";
+drop table "reader";
