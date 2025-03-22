cef7339bb368b22c32dff9fbd9a22c822d0d7c89
lopkol <nora@guidap.co>
2025-03-22 17:08:18 +0100

    feat: add slonik helpers

--- lib/helpers/slonik_test.ts
+++ lib/helpers/slonik_test.ts
@@ -1,5 +1,11 @@
 import { assertEquals } from "@std/assert/equals";
-import { isUniqueConstraintError } from "@/helpers/slonik.ts";
+import {
+  getForeignKeyConstraintErrorData,
+  getInvalidSyntaxErrorData,
+  isForeignKeyConstraintError,
+  isInvalidSyntaxError,
+  isUniqueConstraintError,
+} from "@/helpers/slonik.ts";
 import { describe, it } from "@std/testing/bdd";
 
 describe("isUniqueConstraintError", () => {
@@ -28,5 +34,79 @@ describe("isUniqueConstraintError", () => {
       isUniqueConstraintError({ "cause": { "code": "23502" } }),
       false,
     );
+  });
+});
+
+describe("isForeignKeyConstraintError", () => {
+  it("returns true if the error is a foreign key constraint error", () => {
+    assertEquals(
+      isForeignKeyConstraintError({ "cause": { "code": "23503" } }),
+      true,
+    );
+  });
+
+  it("returns false if the error is not a database error", () => {
+    assertEquals(isForeignKeyConstraintError(new Error("test")), false);
+  });
+
+  it("returns false if the error is a database error but has a different code", () => {
+    assertEquals(
+      isForeignKeyConstraintError({ "cause": { "code": "23502" } }),
+      false,
+    );
+  });
+});
+
+describe("getForeignKeyConstraintErrorData", () => {
+  it("returns the data if the error is a foreign key constraint error", () => {
+    const error = {
+      cause: {
+        code: "23503",
+        detail:
+          'Key (reader_id)=(0195bda3-0273-700d-8a4c-a2548e5a5888) is not present in table "reader".',
+      },
+    };
+
+    assertEquals(
+      getForeignKeyConstraintErrorData(error),
+      { reader_id: "0195bda3-0273-700d-8a4c-a2548e5a5888" },
+    );
+  });
+
+  it("returns null if the error is not a foreign key constraint error", () => {
+    assertEquals(getForeignKeyConstraintErrorData(new Error("test")), null);
+  });
+});
+
+describe("isInvalidSyntaxError", () => {
+  it("returns true if the error is an invalid syntax error", () => {
+    assertEquals(
+      isInvalidSyntaxError({
+        message: 'invalid input syntax for type integer: "test"',
+      }),
+      true,
+    );
+  });
+
+  it("returns false if the error has a different message", () => {
+    assertEquals(
+      isInvalidSyntaxError({ message: "some error message" }),
+      false,
+    );
+  });
+});
+
+describe("getInvalidSyntaxErrorData", () => {
+  it("returns the data if the error is an invalid syntax error", () => {
+    assertEquals(
+      getInvalidSyntaxErrorData({
+        message: 'invalid input syntax for type integer: "test"',
+      }),
+      '"test"',
+    );
+  });
+
+  it("returns null if the error is not an invalid syntax error", () => {
+    assertEquals(getInvalidSyntaxErrorData(new Error("test")), null);
   });
 });
