5b982af4f3aedb197ea843a04b4da3d88485fe53
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-05-18 22:10:55 +0200

    feat: add validators for URL params & query + use new ValidationException

--- api/middlewares/validator.ts
+++ api/middlewares/validator.ts
@@ -1,12 +1,33 @@
 import type z from "zod";
 import { validator } from "hono/validator";
 import type { Context } from "hono";
+import { ValidationException } from "@sffvektor/lib";
 
 export function validateBody(schema: z.ZodSchema) {
   return validator("form", async (_, c: Context) => {
-    const parsed = schema.safeParse(await c.req.json());
+    const parsed = await schema.safeParseAsync(await c.req.json());
     if (!parsed.success) {
-      return c.json(parsed.error, 400);
+      throw new ValidationException(parsed.error);
+    }
+    return parsed.data;
+  });
+}
+
+export function validateQuery(schema: z.ZodSchema) {
+  return validator("query", async (_, c: Context) => {
+    const parsed = await schema.safeParseAsync(c.req.query());
+    if (!parsed.success) {
+      throw new ValidationException(parsed.error);
+    }
+    return parsed.data;
+  });
+}
+
+export function validateParams(schema: z.ZodSchema) {
+  return validator("param", async (_, c: Context) => {
+    const parsed = await schema.safeParseAsync(c.req.param());
+    if (!parsed.success) {
+      throw new ValidationException(parsed.error);
     }
     return parsed.data;
   });
