09c51cd8e984772486b0890b3f68518e22ef672c
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-05-18 15:06:19 +0200

    feat: prevent React Query to retry queries when error is not a 5xx
    I added this because throwing 403 (forbidden) error from the API was creating a bunch of useless retries.

--- frontend/src/components/react-query-provider.tsx
+++ frontend/src/components/react-query-provider.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { AxiosError } from "axios";
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() =>
@@ -9,6 +10,16 @@ const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
       defaultOptions: {
         queries: {
           refetchOnWindowFocus: false,
+          retry: (_failureCount, error) => {
+            // Never retry on 4xx errors
+            if (
+              error instanceof AxiosError && error.status &&
+              error.status >= 400 && error.status < 500
+            ) {
+              return false;
+            }
+            return true;
+          },
         },
       },
     })
