6e38e20dbd1ecb7393d913a96608df37710cc1d2
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-05-18 22:27:34 +0200

    refactor: update authors APIs to match new convention

--- api/routes/authors.ts
+++ api/routes/authors.ts
@@ -12,23 +12,23 @@ import {
 } from "@sffvektor/lib";
 import { validateBody } from "@/middlewares/validator.ts";
 import { isUserAdminMiddleware } from "@/middlewares/role-check.ts";
+import { HttpStatusCode } from "@/helpers/http-code.ts";
+import { mapExceptions } from "@/middlewares/map-exceptions.ts";
 
 const createAuthorApiSchema = createAuthorSchema.strict();
 
 const updateAuthorApiSchema = updateAuthorSchema.strict();
 
-app.get("/api/authors/:id", async (c) => {
-  const pool = await getOrCreateDatabasePool();
-  try {
-    return c.json(await getAuthorById(pool, c.req.param("id")), 200);
-  } catch (error) {
-    if (error instanceof EntityNotFoundException) {
-      return c.json({ message: error.message, details: error.details }, 404);
-    }
-    console.error(error);
-    throw error;
-  }
-});
+app.get(
+  "/api/authors/:id",
+  mapExceptions(
+    [EntityNotFoundException, HttpStatusCode.NotFound],
+  ),
+  async (c) => {
+    const pool = await getOrCreateDatabasePool();
+    return c.json(await getAuthorById(pool, c.req.param("id")));
+  },
+);
 
 app.post(
   "/api/authors",
@@ -36,13 +36,7 @@ app.post(
   validateBody(createAuthorApiSchema),
   async (c) => {
     const pool = await getOrCreateDatabasePool();
-    try {
-      const author = await createAuthor(pool, c.req.valid("form"));
-      return c.json(author, 201);
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    return c.json(await createAuthor(pool, c.req.valid("form")), 201);
   },
 );
 
@@ -50,33 +44,28 @@ app.patch(
   "/api/authors/:id",
   isUserAdminMiddleware,
   validateBody(updateAuthorApiSchema),
+  mapExceptions(
+    [InvalidArgumentException, HttpStatusCode.BadRequest],
+    [EntityNotFoundException, HttpStatusCode.NotFound],
+  ),
   async (c) => {
     const pool = await getOrCreateDatabasePool();
-    try {
-      return c.json(
-        await updateAuthor(pool, c.req.param("id"), c.req.valid("form")),
-        200,
-      );
-    } catch (error) {
-      if (error instanceof InvalidArgumentException) {
-        return c.json({ message: error.message, details: error.details }, 400);
-      }
-      if (error instanceof EntityNotFoundException) {
-        return c.json({ message: error.message, details: error.details }, 404);
-      }
-      console.error(error);
-      throw error;
-    }
+    return c.json(
+      await updateAuthor(
+        pool,
+        c.req.param("id"),
+        c.req.valid("form"),
+      ),
+    );
   },
 );
 
-app.delete("/api/authors/:id", isUserAdminMiddleware, async (c) => {
-  const pool = await getOrCreateDatabasePool();
-  try {
+app.delete(
+  "/api/authors/:id",
+  isUserAdminMiddleware,
+  async (c) => {
+    const pool = await getOrCreateDatabasePool();
     await deleteAuthor(pool, c.req.param("id"));
     return c.json({ message: "Author deleted" }, 200);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-});
+  },
+);
