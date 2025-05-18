7525e8641487d66714de8f3bc66a7d2f4bf10e93
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-05-18 22:23:44 +0200

    refactor: update users APIs to use new middlewares

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -13,6 +13,8 @@ import {
 } from "@sffvektor/lib";
 import { validateBody } from "@/middlewares/validator.ts";
 import { isUserAdminMiddleware } from "@/middlewares/role-check.ts";
+import { HttpStatusCode } from "@/helpers/http-code.ts";
+import { mapExceptions } from "@/middlewares/map-exceptions.ts";
 
 const createUserApiSchema = createUserSchema.strict();
 
@@ -20,45 +22,31 @@ const updateUserApiSchema = updateUserSchema.strict();
 
 app.get("/api/users", isUserAdminMiddleware, async (c) => {
   const pool = await getOrCreateDatabasePool();
-  try {
-    return c.json(await getAllUsers(pool), 200);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return c.json(await getAllUsers(pool), 200);
 });
 
-app.get("/api/users/:id", isUserAdminMiddleware, async (c) => {
-  const pool = await getOrCreateDatabasePool();
-  try {
+app.get(
+  "/api/users/:id",
+  isUserAdminMiddleware,
+  mapExceptions(
+    [EntityNotFoundException, HttpStatusCode.NotFound],
+  ),
+  async (c) => {
+    const pool = await getOrCreateDatabasePool();
     return c.json(await getUserById(pool, c.req.param("id")), 200);
-  } catch (error) {
-    if (error instanceof EntityNotFoundException) {
-      return c.json({ message: error.message, details: error.details }, 404);
-    }
-    console.error(error);
-    throw error;
-  }
-});
+  },
+);
 
 app.post(
   "/api/users",
   isUserAdminMiddleware,
   validateBody(createUserApiSchema),
+  mapExceptions(
+    [UniqueConstraintException, HttpStatusCode.BadRequest],
+  ),
   async (c) => {
     const pool = await getOrCreateDatabasePool();
-    try {
-      return c.json(
-        await createUser(pool, c.req.valid("form")),
-        201,
-      );
-    } catch (error) {
-      if (error instanceof UniqueConstraintException) {
-        return c.json({ message: error.message, details: error.details }, 400);
-      }
-      console.error(error);
-      throw error;
-    }
+    return c.json(await createUser(pool, c.req.valid("form")), 201);
   },
 );
 
@@ -66,6 +54,10 @@ app.patch(
   "/api/users/:id",
   isUserAdminMiddleware,
   validateBody(updateUserApiSchema),
+  mapExceptions(
+    [InvalidArgumentException, HttpStatusCode.BadRequest],
+    [EntityNotFoundException, HttpStatusCode.NotFound],
+  ),
   async (c) => {
     const userId = c.req.param("id");
     const userParams = c.req.valid("form");
@@ -77,23 +69,6 @@ app.patch(
     }
 
     const pool = await getOrCreateDatabasePool();
-    try {
-      return c.json(
-        await updateUser(pool, userId, userParams),
-        201,
-      );
-    } catch (error) {
-      if (
-        error instanceof UniqueConstraintException ||
-        error instanceof InvalidArgumentException
-      ) {
-        return c.json({ message: error.message, details: error.details }, 400);
-      }
-      if (error instanceof EntityNotFoundException) {
-        return c.json({ message: error.message, details: error.details }, 404);
-      }
-      console.error(error);
-      throw error;
-    }
+    return c.json(await updateUser(pool, userId, userParams));
   },
 );
