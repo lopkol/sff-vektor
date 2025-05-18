7bf00ca5a7cc411a80a268fe13086d68a44e9382
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-05-18 22:24:18 +0200

    refactor: update readers APIs to match new convention

--- api/routes/readers.ts
+++ api/routes/readers.ts
@@ -3,10 +3,5 @@ import { getAllReaders, getOrCreateDatabasePool } from "@sffvektor/lib";
 
 app.get("/api/readers", async (c) => {
   const pool = await getOrCreateDatabasePool();
-  try {
-    return c.json(await getAllReaders(pool), 200);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return c.json(await getAllReaders(pool), 200);
 });
