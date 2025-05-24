c4672cd0b071c0c8a265ea9963dd2f7c650f2dc6
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-05-24 23:18:30 +0200

    refactor(backend): setup new logger in API & integration tests

--- api/main.ts
+++ api/main.ts
@@ -1,29 +1,44 @@
-import { getOrCreateDatabasePool, loadEnv, runDbmate } from "@sffvektor/lib";
+import {
+  getOrCreateDatabasePool,
+  loadEnv,
+  logger,
+  type LogLevel,
+  runDbmate,
+  setupLogger,
+} from "@sffvektor/lib";
 import { initDefaultAdminUser } from "@/config/init-users.ts";
 
+await setupLogger({
+  level: Deno.env.get("LOG_LEVEL") as LogLevel || "debug",
+  prettyPrint: ["true", true].includes(
+    Deno.env.get("LOG_PRETTY_PRINT") ?? true,
+  ),
+  useColors: ["true", true].includes(Deno.env.get("LOG_USE_COLORS") ?? true),
+});
+
 await loadEnv();
-console.log("env loaded");
+logger.info("Env loaded");
 
 await getOrCreateDatabasePool();
-console.log("database loaded");
+logger.info("Database loaded");
 
 const { app } = await import("@/config/application.ts");
-console.log("app loaded");
+logger.info("App loaded");
 
 await import("@/routes/index.ts");
-console.log("routes loaded");
+logger.info("Routes loaded");
 
 Deno.serve({
   port: +(Deno.env.get("PORT") ?? 3000),
   onListen: async ({ hostname, port }) => {
-    console.log(`Server started at http://${hostname}:${port}`);
+    logger.info(`Server started listening on http://${hostname}:${port}`);
     if (
       [true, "true"].includes(
         Deno.env.get("DATABASE_RUN_MIGRATIONS") ?? "false",
       )
     ) {
-      console.log("migrations loaded");
-      await runDbmate("up");
+      logger.info("Migrations loaded");
+      await runDbmate("up", ["--no-dump-schema"]);
     }
 
     await initDefaultAdminUser();
--- lib/mod.ts
+++ lib/mod.ts
@@ -24,6 +24,7 @@ export * from "@/schema/user.ts";
 export * from "@/helpers/crypto.ts";
 export * from "@/helpers/migrations.ts";
 export * from "@/helpers/type.ts";
+export * from "@/helpers/logger.ts";
 
 export * from "@/services/moly/book-list.ts";
 export * from "@/services/moly/book.ts";
